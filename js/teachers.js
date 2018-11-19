87f75b67c4ed0960dd72152b0d176f88e42983c9
David Martinez <davmartinez@wl.k12.ia.us>
2018-11-19 15:46:20 -0600

    changed the words and pasted the new one

---
+++ js/teachers.js
@@ -0,0 +1,44 @@
+var teacher = [
+    {
+        image: "buckley.jpg",
+        name: "Susan",
+        subjects: ["Math", "Geometry"],
+        school: "High school",
+        teacher_id: "1234",
+    },
+    {
+        image: "czarnecki.jpg",
+        name: "Mike",
+        subjects: ["Physical Education"],
+        school: "High school",
+        teacher_id: "5678",
+    },
+    {
+        image: "larson.jpg",
+        name: "Melia",
+        subjects: ["English"],
+        school: "High school",
+        teacher_id: "1357",
+    },
+    {
+        image: "gunn.jpg",
+        name: "Mike",
+        subjects: ["Science"],
+        school: "High school",
+        teacher_id: "0987",
+    },
+    {
+        image: "hood.jpg",
+        name: "Betsy",
+        subjects: ["Family and consumer science"],
+        school: "High school",
+        teacher_id: "5674",
+    },
+    {
+        image: "bartleson.jpg",
+        name: "Dan",
+        subjects: ["Math"],
+        school: "High school",
+        teacher_id: "1092",
+    },
+];
